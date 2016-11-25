f74652e51efde03daf10820913ca0ed437b476de
plazarov <lazarow.bg@gmail.com>
2016-11-25 15:03:00 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Spree::Core::Engine.add_routes do
-  devise_for :spree_user,
-             class_name: Spree::User,
-             only: [:omniauth_callbacks],
-             controllers: { omniauth_callbacks: 'spree/omniauth_callbacks' },
-             path: Spree::SocialConfig[:path_prefix]
+  devise_scope :spree_user do
+    match "/users/auth/:action/callback",
+      constraints: { action: /google|facebook|amazon/ },
+      to: "omniauth_callbacks",
+      as: :spree_user_omniauth_callback_backup,
+      via: [:get, :post]
+  end
+  
   resources :user_authentications
 
   get 'account' => 'users#show', as: 'user_root'
